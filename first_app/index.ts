a88094b290b02c0a8e29d984529e73daecf76c3d
xheartean <info@gokhankoc.pl>
2019-11-11 19:00:19 +0100

    implementation of Interface

--- first_app/index.ts
+++ first_app/index.ts
@@ -2,6 +2,24 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url).then( response => {
-    console.log(response.data);
-})
+    // Shows up errors if structure is not matching Todo
+    const todo = response.data as Todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`
+        The todo with ID: ${id}
+        Has a title of: ${title}
+        Is it finished? ${completed}
+    `);
+});
+
